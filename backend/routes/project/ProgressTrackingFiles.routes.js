e682be8e919601ab1e9f9a798d99f6365d512ffd
Devaraj <devaraj15.n@outlook.com>
2026-02-16 23:54:19 +0530

    Refresh Issue

--- backend/routes/project/ProgressTrackingFiles.routes.js
+++ backend/routes/project/ProgressTrackingFiles.routes.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const controller = require('../../controllers/project/ProgressTrackingFiles.controller');
 const auth = require('../../middlewares/auth.middleware');
+const upload = require("../../middlewares/upload.middleware"); // multer
 
 router.use(auth);
 
-router.post('/', controller.create);
+router.post('/',upload.array("files"), controller.create);
 router.get('/', controller.findAll);
 router.get('/:id', controller.findById);
 router.put('/:id', controller.update);
